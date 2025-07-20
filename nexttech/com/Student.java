57a570e386b411898a2a88ebdc94324dcddfe6be
Ramdeo Ramroop <TestProject>
2025-07-20 18:55:38 -0400

    Just a Test Commit

---
+++ nexttech/com/Student.java
@@ -0,0 +1,30 @@
+package nexttech.com;
+
+public class Student {
+	
+	private String name;
+	private int age;
+	private int studentGrade;
+	private int studentSSN;
+	
+	
+	public int getStudentGrade() {
+		return studentGrade;
+	}
+	public void setStudentGrade(int studentGrade) {
+		this.studentGrade = studentGrade;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
